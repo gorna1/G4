3da4f51cc6986737d9ece937fcf738d193ac638f
lucas <lucas@eduroamnat53-184.eduroamnat.uv.es>
2020-10-29 11:53:15 +0100

    club

---
+++ LigaAjedrez/src/ligaajedrez_Model/Club.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ligaajedrez_Model;
+
+/**
+ *
+ * @author lucas
+ */
+public class Club {
+    private String nombre;
+    
+}
